c738ace8812a906a9d2a289b294e4daa12eb1f80
alexander-daniel <alex.vados@gmail.com>
2018-01-26 13:44:27 -0500

    add proper static handler

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 const { send } = require('micro');
-const cors = require('micro-cors')()
-const { router, get } = require('microrouter')
+const cors = require('micro-cors')();
+const { router, get } = require('microrouter');
+const ecstatic = require('ecstatic');
+const staticHandler = (req, res) => {
+  return new Promise(resolve => {
+    ecstatic({ root: `${__dirname}/frontend/static` })(req, res, resolve);
+  });
+};
 
 const search = async (req, res) => {
   try {
@@ -11,21 +17,23 @@ const search = async (req, res) => {
   catch (err) {
     send(res, 400, err.message);
   }
-}
+};
 
-const staticHandler = async (req, res) => {
-  const filePath = req.url === '/' ? 'index.html' : req.url;
+const renderIndex = async (req, res) => {
   try {
-    const data = fs.readFileSync(`${__dirname}/frontend/static/${filePath}`, 'utf8');
+    const data = fs.readFileSync(`${__dirname}/frontend/static/index.html`, 'utf8');
     send(res, 200, data);
   }
 
   catch (err) {
     send(res, 404, 'not found');
   }
-}
+};
 
 module.exports = router(
   get('/api/search/:searchTerm', cors(search)),
+  get('/cool', renderIndex),
+  get('/foo', renderIndex),
+  get('/biz', renderIndex),
   get('/*', staticHandler)
-)
+);
